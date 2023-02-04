09b04bc9cee1d07ce303fded83ef5cbc27e66bcb
ChuckTheBoss <collin.mathews@gmail.com>
2023-02-03 19:59:39 -0700

    completed even or odd, multiply, and opposite number. Added ideal solutions as well

---
+++ multiply.js
@@ -0,0 +1,10 @@
+//This code does not execute properly. Try to figure out why.
+
+// function multiply(a, b){
+//     a * b
+//   }
+
+// No return statement
+function multiply(a, b) {
+    return a * b
+}
---
+++ opposite number.js
@@ -0,0 +1,7 @@
+//Very simple, given an integer or a floating-point number, find its opposite.
+
+//e.g. 1: -1
+
+function opposite(number) {
+    return (-number);
+}
