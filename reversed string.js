eb8452b1892834c359812b24d43aaa89c561df29
ChuckTheBoss <collin.mathews@gmail.com>
2023-02-06 15:23:03 -0700

    Add number to string and reversed string

---
+++ reversed string.js
@@ -0,0 +1,14 @@
+// Complete the solution so that it reverses the string passed into it.
+
+// 'world'  =>  'dlrow'
+// 'word'   =>  'drow'
+
+function solution(str) {
+    let reversedStr = [];
+    for (i = str.length - 1; i >= 0; i--) {
+        reversedStr.push(str[i]);
+    }
+    return reversedStr.join("");
+}
+
+console.log(solution("world"));
