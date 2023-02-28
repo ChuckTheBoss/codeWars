3b8a524192082320039f907525724f9bf2c912c1
ChuckTheBoss <collin.mathews@gmail.com>
2023-02-27 18:12:21 -0700

    add 101 dalmations and flower exercise

---
+++ 101 dalmations.js
@@ -0,0 +1,27 @@
+// fix this: 
+// function howManyDalmations(number) :
+
+//     var dogs ["Hardly any", "More than a handful!", "Woah that's a lot of dogs!", "101 DALMATIONS!!!"];
+
+//     var respond = number <= 10 ? dogs[0] (number <= 50 ? dogs[1] : (number = 101  dogs[3] : dogs[2]
+
+//   return respond
+
+function howManyDalmatians(number) {
+
+    let dogs = ["Hardly any", "More than a handful!", "Woah that's a lot of dogs!", "101 DALMATIANS!!!"];
+
+    let respond = number <= 10 ? dogs[0] :
+        (number <= 50 ? dogs[1] :
+            (number === 101 ?
+                dogs[3] : dogs[2]));
+
+    return respond
+};
+
+console.log(howManyDalmatians(26));
+console.log(howManyDalmatians(8));
+console.log(howManyDalmatians(14));
+console.log(howManyDalmatians(80));
+console.log(howManyDalmatians(100));
+console.log(howManyDalmatians(101));
